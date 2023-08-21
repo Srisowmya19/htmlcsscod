c05ef71b42dc709a0111f30b1b1469ae4139ff91
Srisowmya19 <142646430+Srisowmya19@users.noreply.github.com>
2023-08-21 07:34:33 +0000

    sowmya

---
+++ surya.js
@@ -0,0 +1,10 @@
+console.log("hellow world");
+console.log(2+3)
+let a="hellow";
+let a1;
+a1="hellow";
+
+let x="20";
+console.log(typeof(x)); //string
+let y=parseInt(x);
+console.log(typeof(y)); //number
